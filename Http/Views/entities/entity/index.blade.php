87fb467dc0a10720ea61ffb7c75543a3641b2fdd
Ramon Linares <rlinareslf@gmail.com>
2024-02-22 16:10:01 -0400

    Actualizacion y avances

--- Http/Views/entities/entity/index.blade.php
+++ Http/Views/entities/entity/index.blade.php
@@ -24,9 +24,30 @@
                         <td>{{$row->razonSocialEmisor()}}</td>
                         <td>{{$row->montoTotal()}}</td>
                         <td class="text-end py-0 pt-1">
-                            <button type="button" class="btn btn-sm btn-outline-secondary rounded-pill px-3">
-                                Acion
-                            </button>
+                            <div class="dropdown dropstart nopointer">
+                                <button class="btn btn-sm btn-secondary rounded-pill px-3 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Acion
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <h6 class="dropdown-header">{{$row->razonSocialEmisor()}}</h6>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <span class="mdi mdi-link mdi-20px"></span>
+                                            Envíar Aprobación Comercial
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <span class="mdi mdi-link mdi-20px"></span> Another action
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <span class="mdi mdi-link mdi-20px"></span> Something else here
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </td>
                     </tr>
                     @endforeach
